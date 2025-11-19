976c0ccc598d3a10513ed1f316ac8c8b954d5b46
kdanni <kdanni@kdanni.hu>
2025-11-19 23:30:37 +0100

    app.options(/(.*)/, cors(corsOptions)); fix

--- src/main/express.mjs
+++ src/main/express.mjs
@@ -44,7 +44,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // DO NOT use app.options('*', cors(corsOptions));
-app.options('/', cors(corsOptions));
+app.options(/(.*)/, cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req, res) => {
